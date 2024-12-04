7cb55c329034a51bc04ce17c9d47d041c211cf80
Nikita <nikita119@protonmail.com>
2024-12-04 13:27:42 +0300

    meal detail page & save meal function and page

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,9 +2,15 @@ import classes from "./page.module.css";
 import Link from "next/link";
 import MealsGrid from "@/components/meals/meals-grid";
 import {getMeals} from "@/lib/meals";
+import {Suspense} from "react";
 
-export default  async function MealsPage() {
+
+async function Meals() {
     const meals = await getMeals();
+    return <MealsGrid meals={meals} />
+}
+
+export default function MealsPage() {
     return (
        <>
            <header className={classes.header}>
@@ -14,7 +20,7 @@ export default  async function MealsPage() {
                    <Link href='/meals/share'>Share your favorite recipe</Link>
                </p>
            </header>
-           <main className={classes.main}><MealsGrid meals={meals} /></main>
+           <main className={classes.main}><Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}><Meals /></Suspense></main>
        </>
     )
 }
